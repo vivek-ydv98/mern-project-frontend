9e7fc599e99dbfd5d7ee466d80feeef21f7b7acb
vivek <vivekyadav8101998@gmail.com>
2023-09-02 21:11:52 +0530

    login-signup

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login } from '../features/auth/components/Login'
+
+export default function LoginPage() {
+  return (
+    <div>LoginPage
+    <Login></Login>
+    </div>
+  )
+}
