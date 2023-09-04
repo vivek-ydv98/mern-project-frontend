e78c5031affef8a5dcab0529ce0cd2363641851e
vivek <vivekyadav8101998@gmail.com>
2023-09-04 23:58:36 +0530

    product page changes and filters

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -6,11 +6,22 @@ export function fetchAllProducts() {
     resolve({ data });
   });
 }
-export function fetchProductsByFilter(filter) {
-  //filter ={"category":"smartphone"}
+export function fetchProductsByFilter(filter,sort) {
+  //filter ={"category":["smartphone","smartphone","laptop"]}
+  // sort = {_sort:"price",_order="desc"}
+
   let queryString = "";
   for (let key in filter) {
-    queryString += `${key}=${filter[key]}&`;
+    const categoryValues = filter[key];
+
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`;
   }
 
   return new Promise(async (resolve) => {
