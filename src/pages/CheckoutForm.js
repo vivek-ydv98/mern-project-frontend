723c0ddf0313646d15090d81dcf3f9e9a791ce4b
vivek <vivekyadav8101998@gmail.com>
2023-10-05 01:43:46 +0530

    test

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -55,7 +55,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-dsm8.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `https://mern-ecommerce-chi.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
